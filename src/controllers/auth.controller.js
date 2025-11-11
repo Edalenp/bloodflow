5ca636474e7a153042e4d49d9d0f59c56839ca33
santiagoQP31 <santiagoqp3103@gmail.com>
2025-11-11 18:22:28 -0500

    Update: Lista la logica de negocio para registrar y autenticar usuarios

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,4 +8,67 @@ export const register = async (req, req) => {
 
     if (!email || !password)
         return res.status(400).json({ message: 'Email and password required' });
-}
+
+    try {
+        const pool = await poolPromise;
+
+        // Check if user already exists
+        const existingUser = await pool
+            .request()
+            .input('email', email)
+            .query('SELECT id FROM useres WHERE email = @email');
+
+        if (existingUser.recordset.length > 0)
+            return res.status(400).json({ message: 'User already exists' });
+
+        const hashed = await hashPassword(password);
+        const newId = uuidv4();
+
+        await pool
+            .request()
+            .input('id', newId)
+            .input('email', email)
+            .input('password', hashed)
+            .input('role', role)
+            .query(`
+                INSERT INTO users (id, email, password, role) 
+                VALUES (@id, @email, @password, @role)
+            `);
+
+        return res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Register errror:', err);
+        return res.status(500).json({ message: 'Server error' });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+        return res.status(400).json({ message: 'Email and password required' });
+
+    try {
+        const pool = await poolPromise;
+
+        const result = await pool
+            .request()
+            .input('email', email)
+            .query('SELECT id, password, role FROM users WHERE email = @email');
+
+        if (result.recordset.length === 0)
+            return res.status(401).json({ message: 'Invalid credentials' });
+
+        const accessToken = generateAcessToken({ id: user.id, role: user.role });
+        const refreshToken = generateRefreshToken({ id: user.id });
+
+        return res.status(200).json({
+            accessToken,
+            refreshToken,
+            user: { id: user.id, email, role: user.role }
+        });
+    } catch (error) {
+        console.error('Login error:', err);
+        return res.status(500).json({ message: 'Server error' });
+    }
+};
